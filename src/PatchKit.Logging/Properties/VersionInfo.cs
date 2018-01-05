105f145d4d393691d3cac5afa5e1995f1029595b
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2018-01-05 17:50:03 +0100

    Change NuGet usage

--- src/PatchKit.Logging/Properties/VersionInfo.cs
+++ src/PatchKit.Logging/Properties/VersionInfo.cs
@@ -2,6 +2,6 @@
 {
     public const string Major = "1";
     public const string Minor = "0";
-    public const string Patch = "0";
+    public const string Patch = "1";
     public const string Suffix = "";
 }
