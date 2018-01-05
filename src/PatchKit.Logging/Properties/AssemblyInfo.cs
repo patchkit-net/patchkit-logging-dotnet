732f053446137306a219842398265862930b74cd
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2018-01-05 02:22:47 +0100

    Integrate project with NuGet

--- src/PatchKit.Logging/Properties/AssemblyInfo.cs
+++ src/PatchKit.Logging/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -7,30 +6,17 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("PatchKit.Logging")]
 [assembly: AssemblyDescription("Shared library with standarized interfaces for creating application logs.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Upsoft")]
 [assembly: AssemblyProduct("PatchKit.Logging")]
-[assembly: AssemblyCopyright("Copyright © Upsoft 2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Copyright © Upsoft 2018")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("C2197D71-3149-4A2F-9D3F-F0134790894D")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+#if DEBUG
+[assembly: AssemblyVersion(VersionInfo.Major + "." + VersionInfo.Minor + ".*")]
+#else
+[assembly: AssemblyInformationalVersion(VersionInfo.Major + "." + VersionInfo.Minor + "." + VersionInfo.Patch + VersionInfo.Suffix)]
+#endif
